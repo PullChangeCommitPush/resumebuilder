da935622f9cb910c0eca9ba827f0b9931af83722
PullChangeCommitPush <dhananjaymulay95@gmail.com>
2021-01-14 15:37:22 +0530

    set up2

--- db.js
+++ db.js
@@ -4,9 +4,11 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
-var DB_URL = process.env.MONGODB_URL || "mongodb+srv://danny:danny123@cluster0.5rdyu.mongodb.net/sample_training?retryWrites=true&w=majority"
-mongoose.connect(DB_URL);
+// var DB_URL = process.env.MONGODB_URL || "mongodb+srv://danny:danny123@cluster0.5rdyu.mongodb.net/sample_training?retryWrites=true&w=majority"
+// mongoose.connect(DB_URL);
 
+var DB_URL = process.env.MONGODB_URL || 'mongodb://localhost/employees';
+mongoose.connect(DB_URL);
 
 mongoose.connection.once('open',(err)=>{
     if(!err)
